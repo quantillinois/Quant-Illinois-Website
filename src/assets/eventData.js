6dfd6b1ad3fd532bd6ca0a798484e5e239cbdd24
Abhay Benoy <abhihome111@gmail.com>
2025-03-04 23:18:48 -0600

    updated events tab

--- src/assets/eventData.js
+++ src/assets/eventData.js
@@ -1,16 +1,9 @@
 export const eventData = [
   {
-    datetime: "Tue, Jan 23th 2025 - 6:00 PM CST",
-    title: "Info Night",
-    location: "Siebel Center for Design - Classroom 1000/1002",
-    description: "Learn about Quant Illinois and how to get involved.",
-    // rsvpLink: "#"
-  },
-  {
-    datetime: "Date: TBD",
+    datetime: "Sat, Apr 5th 2025 - 8:00 AM CST",
     title: "UIUC Trading Competition",
-    location: "TBD",
-    description: "Network with industry professionals and compete for prizes in day of trading games.",
-    // rsvpLink: "#"
+    location: "Siebel Center for Design",
+    description: "Network with industry professionals and compete for a $5000 prize pool in a full day of trading games.",
+    rsvpLink: "https://lnkd.in/eT3ru9vq"
   },
 ]
