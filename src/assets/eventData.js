224b622f61591c6317f8552efb49f3cfd09558b7
Chris <chris.rsuarez19@gmail.com>
2026-03-09 17:38:37 -0500

    trying to upload event again.

--- src/assets/eventData.js
+++ src/assets/eventData.js
@@ -1,4 +1,11 @@
 export const eventData = [
+  {
+    datetime: " March, 13th 2026 - 11:59 PM CST",
+    title: "Spring 2026 Trading Competition ",
+    description: "Join us for TWO full days of competitive trading games, networking with top firms, and company-sponsored events. April(18th-19th)",
+    rsvpLink: "https://forms.gle/GW2HoAz8AdkeE4Dk6",
+    msg: "Apply"
+  },
   // {
   //   datetime: "Sat, Aug 30th 2025 - 3:00 PM CST",
   //   title: "Spring 2026 Info Night",
@@ -7,13 +14,7 @@ export const eventData = [
   //   rsvpLink: "https://discord.gg/xnjdcgteKR",
   //   msg: "Discord"
   // },
-    {
-    datetime: " March, 13th 2026 - 11:59 PM CST",
-    title: "Spring 2026 Trading Competition ",
-    description: "Join us for TWO full days of competitive trading games, networking with top firms, and company-sponsored events. April(18th-19th)",
-    rsvpLink: "https://forms.gle/GW2HoAz8AdkeE4Dk6",
-    msg: "Apply"
-  }
+    
   //   {
   //   datetime: "Monday, Jan 26th 2026 - 11:59 PM CST",
   //   title: "Spring 2026 Software Divison Application",
